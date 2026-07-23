bcb5dbf8dbac2a6aa1f320bed5387a7fa0ea7025
asitos <4shut0shparashar@gmail.com>
2026-07-23 18:36:42 +0530

    chore(telemetry): update honeypot state

--- sync_state.sh
+++ sync_state.sh
@@ -5,6 +5,8 @@ cd /home/asitos/Projects/rasmalaaiPiAuthSim
 
 # check if the json file was updated since the last push
 if [[ -n $(git status -s captures.json) ]]; then
+    git fetch
+    git pull origin main
     echo "[+] new credentials found. syncing to upstream..."
     git add captures.json
     git commit -m "chore(telemetry): update honeypot state [skip ci]"
